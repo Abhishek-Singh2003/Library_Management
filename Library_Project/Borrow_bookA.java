c001aeee6f66fd0ee3b3624590de5295105388e9
Abhishek-Singh2003 <abhisheksingh13722@gmail.com>
2025-08-01 17:07:13 +0530

    Library_Management

---
+++ Library_Project/Borrow_bookA.java
@@ -0,0 +1,26 @@
+
+public class Borrow_bookA
+{
+private String title;
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getAuthor() {
+	return author;
+}
+public void setAuthor(String author) {
+	this.author = author;
+}
+public int getISBN() {
+	return ISBN;
+}
+public void setISBN(int iSBN) {
+	ISBN = iSBN;
+}
+private String author;
+private int ISBN;
+
+}
